515eb42cb31f9b2c910f6f8989d0beb5006989c7
jadegm <walterjadecastaneda7@gmail.com>
2016-06-02 13:03:54 +0000

    Add files via upload

--- Register.php
+++ Register.php
@@ -1,11 +1,10 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("mysql12.000webhost.com", "a8707779_jade", "imortal123", "a8707779_mobile");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
     $username = $_POST["username"];
     $password = $_POST["password"];
-
     $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
     mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
     mysqli_stmt_execute($statement);
